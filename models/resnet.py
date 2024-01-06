993a4f2943550607d95198dbec340a982499ae91
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2024-01-06 15:44:47 +0100

    Updated resnet file

--- models/resnet.py
+++ models/resnet.py
@@ -5,9 +5,9 @@ def get_resnet(name: str, pretrained=False):
     Imports a ResNet model considering the given model name.
 
     Parameters:
-        name (string): the ResNet model name. It must be one of the allowed
-                       ResNet valid names.
-        pretrained (boolean): Default is False. Allows us to choose an already 
+        name (string): The ResNet-model name. It must be one of the allowed
+                       ResNet valid names: resnet18, resnet34 or resnet50.
+        pretrained (boolean): Default is False. Allows us to choose an already
                               pretrained model or not.
 
     Returns:
@@ -16,9 +16,9 @@ def get_resnet(name: str, pretrained=False):
     resnets = {
         "resnet18": torchvision.models.resnet18(pretrained=pretrained),
         "resnet34": torchvision.models.resnet34(pretrained=pretrained),
-        "resnet50": torchvision.models.resnet50(pretrained=pretrained),
+        "resnet50": torchvision.models.resnet50(pretrained=pretrained)
     }
     if name not in resnets.keys():
-        raise KeyError(f"{name} is not a valid model name !!")
-    
+        raise KeyError(f"{name} is not a valid ResNet-model name !!")
+
     return resnets[name]
