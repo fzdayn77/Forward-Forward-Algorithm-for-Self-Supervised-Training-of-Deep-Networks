ead77e33653f9ace5537c2a13d3ae79044d0b964
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2024-01-06 15:48:43 +0100

    Updated resnet file and added identity file

--- models/simCLR.py
+++ models/simCLR.py
@@ -1,28 +1,36 @@
 import torch.nn as nn
-from models.projection_head import Projection_Head
+from models.identity import Identity
 
 class SimCLR(nn.Module):
-    def __init__(self, encoder: nn.Module, n_features: int, projection: bool=True, device=None):
-        super().__init__()
+    def __init__(self, encoder, projection_dim: int, n_features: int, projection=True):
+        super(SimCLR, self).__init__()
         self.encoder = encoder
         self.n_features = n_features
         self.projection = projection
-        self.device = device
 
-        if self.projection:
-          self.projection_head = Projection_Head(input_dim=n_features, hidden_dim=n_features, 
-                                                 output_dim=128, device=self.device)
-          print("Projection Head ===> Enabled")
-        else:
-          print("Projection Head ===> Disabled")
-
-    def forward(self, x):
-        # x -> h
-        h = self.encoder(x).flatten(start_dim=1)
-
-        if self.projection:
-          # h -> z
-          z = self.projection_head(h)
-          return z
+        # Replace the fc layer with an Identity function
+        self.encoder.fc = Identity()
         
-        return h
+        if self.projection:
+            # We use a MLP with one hidden layer
+            self.projector = nn.Sequential(
+                nn.Linear(self.n_features, self.n_features, bias=False),
+                nn.ReLU(),
+                nn.Linear(self.n_features, projection_dim, bias=False),
+            )
+            print("Projection Head ===> Enabled")
+        else:
+            print("Projection Head ===> Disabled")
+
+    def forward(self, x_i, x_j):
+        # x -> h
+        h_i = self.encoder(x_i)
+        h_j = self.encoder(x_j)
+        
+        if self.projection:
+            # h -> z
+            z_i = self.projector(h_i)
+            z_j = self.projector(h_j)  
+            return h_i, h_j, z_i, z_j
+        
+        return h_i, h_j
