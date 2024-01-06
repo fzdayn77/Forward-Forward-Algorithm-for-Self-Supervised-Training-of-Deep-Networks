d4884fbd92b7b6c41d4768cb6390a5f37ac5a8c7
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2024-01-06 16:55:28 +0100

    Added plot curves function

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,1 +1,12 @@
 import matplotlib as plt
+
+def plot_curves(title="Title", list_1=[], list_1_label="Label 1", 
+                list_2=[], list_2_label="Label 2", x_label="X label", y_label="Y label"):
+    plt.figure(figsize=(10,10))
+    plt.title(title)
+    plt.plot(list_1, label=list_1_label)
+    plt.plot(list_2, label=list_2_label)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.show()
